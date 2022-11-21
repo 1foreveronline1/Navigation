61f1112fec223e554f803fea82b3fab3b9d0fa9b
1foreveronline1 <andrey29082002@gmail.com>
2022-11-21 13:54:11 +0300

    Редактировал структуру кода и исправил по замечаниям

--- Profile/ProfileHeaderView.swift
+++ Profile/ProfileHeaderView.swift
@@ -1,20 +1,97 @@
 import UIKit
 
 class ProfileHeaderView: UIView {
+        
+    let profileView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    let avatarImageView: UIImageView = {
+        let picture = UIImageView()
+        picture.image = UIImage(named: "Image")
+        picture.clipsToBounds = true
+        picture.layer.borderWidth = 3
+        picture.layer.borderColor = UIColor.white.cgColor
+        picture.layer.cornerRadius = 50
+        picture.translatesAutoresizingMaskIntoConstraints = false
+        return picture
+    }()
+    
+    let fullNameLabel: UILabel = {
+        let name = UILabel()
+        name.textAlignment = .left
+        name.font = UIFont.boldSystemFont(ofSize: 18)
+        name.text = "Кот с цепью"
+        name.translatesAutoresizingMaskIntoConstraints = false
+        return name
+    }()
+    
+    let statusLabel: UILabel = {
+        let status = UILabel()
+        status.textAlignment = .left
+        status.font = UIFont.systemFont(ofSize: 14)
+        status.text = "Крутой блин..."
+        status.textColor = .gray
+        status.translatesAutoresizingMaskIntoConstraints = false
+        return status
+    }()
+
+    let statusTextField: UITextField = {
+       let text = UITextField ()
+       text.backgroundColor = .white
+       text.font = .systemFont(ofSize: 15, weight: .regular)
+       text.textColor = .black
+       text.layer.cornerRadius = 12
+       text.borderStyle = .roundedRect
+       text.layer.borderWidth = 1
+       text.clipsToBounds = true
+       text.placeholder = "Ваш статус..."
+       text.keyboardType = .default
+       text.clearButtonMode = .whileEditing
+       text.contentHorizontalAlignment = .center
+       text.returnKeyType = .done
+       text.addTarget(Any?.self, action: #selector(statusTextChanged), for: .editingChanged)
+       text.translatesAutoresizingMaskIntoConstraints = false
+       return text
+   }()
+    
+    let setStatusButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Изменить статус", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 4
+        button.setTitleColor(.white, for: .normal)
+        button.layer.shadowOffset = .init(width: 4, height: 4)
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowRadius = 4
+        button.layer.shadowOpacity = 0.7
+        button.addTarget(Any?.self, action: #selector(statButAction), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+   
+    var statusText: String = ""
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.addSubview(profileView)
-        addMySubview()
+        addSubview(profileView)
+        addViews()
         setupView()
     }
     
-    func addMySubview() {
-        profileView.addSubview(avatarImageView)
-        profileView.addSubview(fullNameLabel)
-        profileView.addSubview(statusLabel)
-        profileView.addSubview(setStatusButton)
-        profileView.addSubview(statusTextField)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addViews() {
+        self.addSubview(avatarImageView)
+        self.addSubview(fullNameLabel)
+        self.addSubview(statusLabel)
+        self.addSubview(statusTextField)
+        self.addSubview(setStatusButton)
     }
     
     func setupView() {
@@ -48,80 +125,6 @@ class ProfileHeaderView: UIView {
         ])
     }
     
-        let profileView: UIView = {
-            let view = UIView()
-            view.backgroundColor = .lightGray
-            view.translatesAutoresizingMaskIntoConstraints = false
-            return view
-        }()
-
-        
-        let avatarImageView: UIImageView = {
-            let picture = UIImageView()
-            picture.image = UIImage(named: "Image")
-            picture.clipsToBounds = true
-            picture.layer.borderWidth = 3
-            picture.layer.borderColor = UIColor.white.cgColor
-            picture.layer.cornerRadius = 50
-            picture.translatesAutoresizingMaskIntoConstraints = false
-            return picture
-        }()
-        
-        let fullNameLabel: UILabel = {
-            let name = UILabel()
-            name.textAlignment = .left
-            name.font = UIFont.boldSystemFont(ofSize: 18)
-            name.text = "Кот с цепью"
-            name.translatesAutoresizingMaskIntoConstraints = false
-            return name
-        }()
-        
-        let statusLabel: UILabel = {
-            let status = UILabel()
-            status.textAlignment = .left
-            status.font = UIFont.systemFont(ofSize: 14)
-            status.text = "Крутой блин..."
-            status.textColor = .gray
-            status.translatesAutoresizingMaskIntoConstraints = false
-            return status
-        }()
-    
-        let statusTextField: UITextField = {
-           let text = UITextField ()
-           text.backgroundColor = .white
-           text.font = .systemFont(ofSize: 15, weight: .regular)
-           text.textColor = .black
-           text.layer.cornerRadius = 12
-           text.borderStyle = .roundedRect
-           text.layer.borderWidth = 1
-           text.clipsToBounds = true
-           text.placeholder = "Ваш статус..."
-           text.keyboardType = .default
-           text.clearButtonMode = .whileEditing
-           text.contentHorizontalAlignment = .center
-           text.returnKeyType = .done
-           text.addTarget(Any?.self, action: #selector(statusTextChanged), for: .editingChanged)
-           text.translatesAutoresizingMaskIntoConstraints = false
-           return text
-       }()
-        
-        let setStatusButton: UIButton = {
-            let button = UIButton()
-            button.setTitle("Изменить статус", for: .normal)
-            button.backgroundColor = .systemBlue
-            button.layer.cornerRadius = 4
-            button.setTitleColor(.white, for: .normal)
-            button.layer.shadowOffset = .init(width: 4, height: 4)
-            button.layer.shadowColor = UIColor.black.cgColor
-            button.layer.shadowRadius = 4
-            button.layer.shadowOpacity = 0.7
-            button.addTarget(Any?.self, action: #selector(statButAction), for: .touchUpInside)
-            button.translatesAutoresizingMaskIntoConstraints = false
-            return button
-        }()
-       
-    var statusText: String = ""
-    
     @objc func statButAction() {
         statusLabel.text = statusText
     }
@@ -130,8 +133,4 @@ class ProfileHeaderView: UIView {
             statusText = textField.text ?? ""
             }
     
-       required init?(coder: NSCoder) {
-           fatalError("init(coder:) has not been implemented")
-       }
-
 }
